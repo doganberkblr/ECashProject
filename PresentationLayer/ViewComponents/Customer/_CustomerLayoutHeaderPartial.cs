f742b66b3c5547ef7c3e62ae768639ed35860a23
Dogan Berk BULUR <doganbrkblr@icloud.com>
2023-08-20 01:37:22 +0300

    20.08.2023 - 2
    ViewComponent

---
+++ PresentationLayer/ViewComponents/Customer/_CustomerLayoutHeaderPartial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutHeaderPartial : ViewComponent
+	{
+	  public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	
+	}
+}
+
