066484bc4fb8ee58e37da6818969e93b79f6aea7
Dogan Berk BULUR <doganbrkblr@icloud.com>
2023-08-20 00:12:57 +0300

    20.08.2023

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -39,7 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Register}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
 
