7a9b4e3eb11d6e6b4a6dccfb348aed3ade56d53c
Dogan Berk BULUR <doganbrkblr@icloud.com>
2023-08-28 00:36:39 +0300

    28.08.2023
    29.part end - Exchange API update

--- PresentationLayer/Controllers/ExchangeController.cs
+++ PresentationLayer/Controllers/ExchangeController.cs
@@ -12,6 +12,7 @@ namespace PresentationLayer.Controllers
     
         public async Task<IActionResult> Index()
         {
+
             #region
             var client = new HttpClient();
             var request = new HttpRequestMessage
@@ -28,7 +29,7 @@ namespace PresentationLayer.Controllers
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                ViewBag.USDtoTRY = body;  
+                ViewBag.USDtoTRY = Decimal.Parse(body.Replace(".", ",")).ToString("0.000");
             }
             #endregion
 
@@ -44,13 +45,54 @@ namespace PresentationLayer.Controllers
         { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
     },
             };
-            using (var response2 = await client2.SendAsync(request))
+            using (var response2 = await client2.SendAsync(request2))
             {
                 response2.EnsureSuccessStatusCode();
                 var body2 = await response2.Content.ReadAsStringAsync();
-                ViewBag.EURtoTRY = body2;
+                ViewBag.EURtoTRY = Decimal.Parse(body2.Replace(".", ",")).ToString("0.000");
             }
             #endregion
+
+            #region
+            var client3 = new HttpClient();
+            var request3 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=GBP&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "dfa2b84090msh775c24893cce8fap154e78jsn5f92f9fefb1b" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response3 = await client3.SendAsync(request3))
+            {
+                response3.EnsureSuccessStatusCode();
+                var body3 = await response3.Content.ReadAsStringAsync();
+                ViewBag.GBPtoTRY = Decimal.Parse(body3.Replace(".", ",")).ToString("0.000");
+            }
+            #endregion
+
+            #region
+            var client4 = new HttpClient();
+            var request4 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=EUR&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "dfa2b84090msh775c24893cce8fap154e78jsn5f92f9fefb1b" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response4 = await client4.SendAsync(request4))
+            {
+                response4.EnsureSuccessStatusCode();
+                var body4 = await response4.Content.ReadAsStringAsync();
+                ViewBag.USDtoEUR = Decimal.Parse(body4.Replace(".", ",")).ToString("0.000");
+            }
+            #endregion
+
             return View();
         }
     }
