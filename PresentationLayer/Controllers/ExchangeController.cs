9955a8aa3a05b6f941bfb7add0e3df9024a6186f
Dogan Berk BULUR <doganbrkblr@icloud.com>
2023-08-27 23:38:19 +0300

    27.08.2023
    28.part end -- Exchange API added.

---
+++ PresentationLayer/Controllers/ExchangeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PresentationLayer.Controllers
+{
+    public class ExchangeController : Controller
+    {
+    
+        public async Task<IActionResult> Index()
+        {
+            #region
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "dfa2b84090msh775c24893cce8fap154e78jsn5f92f9fefb1b" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ViewBag.USDtoTRY = body;  
+            }
+            #endregion
+
+            #region
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=EUR&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "dfa2b84090msh775c24893cce8fap154e78jsn5f92f9fefb1b" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                ViewBag.EURtoTRY = body2;
+            }
+            #endregion
+            return View();
+        }
+    }
+}
+
