5c631744651fb8ab1e14af3f8e3056e60a45c0ad
Dogan Berk BULUR <doganbrkblr@icloud.com>
2023-10-15 23:05:37 +0300

    15.10.2023 - 2

--- DataAccessLayer/EntityFramework/EfCustomerAccountProcessDAL.cs
+++ DataAccessLayer/EntityFramework/EfCustomerAccountProcessDAL.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Abstract;
 using DataAccessLayer.Repositories;
 using ECashProject.DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.EntityFramework
 {
@@ -11,7 +12,7 @@ namespace DataAccessLayer.EntityFramework
         public List<CustomerAccountProcess> MyLastProcess(int id)
         {
             using var context = new Context();
-            var values = context.CustomerAccountProcesses.Where(x => x.ReceiverID == id || x.SenderID == id).ToList();
+            var values = context.CustomerAccountProcesses.Include(y=>y.SenderCustomer).ThenInclude(z=>z.AppUser).Include(w=>w.ReceiverCustomer).Where(x => x.ReceiverID == id || x.SenderID == id).ToList();
             return values;
         }
     }
